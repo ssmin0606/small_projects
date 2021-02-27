942382e70b1b203b665fb5704420bd5a086e4a37
sangmin song <ssmin940606@gmail.com>
2021-02-27 15:59:33 +0900

    210227 - add annotations

--- Clone_Google_momentum/weather.js
+++ Clone_Google_momentum/weather.js
@@ -7,6 +7,8 @@ function getWeather(lat, lng) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
   )
+  // 왜 function()을 계속 만들어서 해주는 것인지?
+  // 동작들을 나눠서 지정해주기 위해서 함수로 나눠준 것인가?
   .then(function(response) {
     return response.json();
   })
@@ -22,7 +24,7 @@ function saveCoords(coords){
   localStorage.setItem(COORDS, JSON.stringify(coords))
 }
 
-function handleGeoSucces(position){
+function handleGeoSuccess(position){
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   const coordsObj = {
@@ -40,7 +42,7 @@ function handleGeoError(){
 }
 
 function askForCoords(){
-  navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
 
 function loadCoords(){
